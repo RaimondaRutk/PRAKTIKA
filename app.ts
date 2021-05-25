7b86f7b09bf24748e569f508217372e4d40ce27a
RaimondaRutk <rrutkauskaite00@gmail.com>
2021-05-25 15:11:33 +0300

    6 uzduotis update

--- app.ts
+++ app.ts
@@ -1,7 +1,36 @@
-function addNumbers(a: number, b: number): number {
+/* function addNumbers(a: number, b: number): number {
   return a + b;
 }
 var sum = addNumbers(10, 15);
 
 document.getElementById('app')?.append('Sum of the two numbers is : ' + sum);
-//console.log('Sum of the two numbers is: ' + sum);
+console.log('Sum of the two numbers is: ' + sum);
+*/
+
+const tourism = [
+  '1',
+  'Traku_pilis',
+  'Malborko_pilis',
+  'Laju_takas',
+  'Kreekenavos_turizmo_centras',
+  'Rundales_pilis',
+  'Smetonos_dvaras',
+];
+
+type Tourism = Array<string>;
+
+function getLithuaniaPlaces(tourism: Tourism, template: Tourism): Tourism {
+  const SetTourism = tourism.filter((place) => template.indexOf(place) > -1);
+  return SetTourism;
+}
+
+var Template = [
+  'Traku_pilis',
+  'Laju_takas',
+  'Kreekenavos_turizmo_centras',
+  'Smetonos_dvaras',
+];
+
+const result = getLithuaniaPlaces(tourism, Template);
+
+document.getElementById('app')?.append(result.toString());
--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
-function addNumbers(a, b) {
+"use strict";
+/* function addNumbers(a: number, b: number): number {
   return a + b;
 }
 var sum = addNumbers(10, 15);
 
-//document.getElementById('ap')?.append('Sum of the two number is: ' + sum);
+document.getElementById('app')?.append('Sum of the two numbers is : ' + sum);
 console.log('Sum of the two numbers is: ' + sum);
+*/
+var _a;
+var tourism = [
+    '1',
+    'Traku_pilis',
+    'Malborko_pilis',
+    'Laju_takas',
+    'Kreekenavos_turizmo_centras',
+    'Rundales_pilis',
+    'Smetonos_dvaras',
+];
+function getLithuaniaPlaces(tourism, template) {
+    var SetTourism = tourism.filter(function (place) { return template.indexOf(place) > -1; });
+    return SetTourism;
+}
+var Template = [
+    'Traku_pilis',
+    'Laju_takas',
+    'Kreekenavos_turizmo_centras',
+    'Smetonos_dvaras',
+];
+var result = getLithuaniaPlaces(tourism, Template);
+(_a = document.getElementById('app')) === null || _a === void 0 ? void 0 : _a.append(result.toString());
