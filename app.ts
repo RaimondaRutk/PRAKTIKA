09c3df735eaab51212d8dfdf030af3d40797f8a6
RaimondaRutk <rrutkauskaite00@gmail.com>
2021-05-25 13:16:07 +0300

    5 uzduotis, pradeta 6

---
+++ app.ts
@@ -0,0 +1,7 @@
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+var sum = addNumbers(10, 15);
+
+document.getElementById('app')?.append('Sum of the two numbers is : ' + sum);
+//console.log('Sum of the two numbers is: ' + sum);
---
+++ js/app.js
@@ -0,0 +1,7 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+var sum = addNumbers(10, 15);
+
+//document.getElementById('ap')?.append('Sum of the two number is: ' + sum);
+console.log('Sum of the two numbers is: ' + sum);
