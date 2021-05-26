9c1bc68c4fb627a0fc0fd5023fe4a38ec0732ee1
RaimondaRutk <rrutkauskaite00@gmail.com>
2021-05-26 23:57:26 +0300

    uzduotis

--- app.ts
+++ app.ts
@@ -74,6 +74,56 @@ const symbolCount = (tourism: Tourism): Tourism => {
   return symbols;
 };
 
+type tourism = Array<string>;
+type TourismObj = {
+  title: string;
+  index: number;
+};
+
+function toObject(tourism: Tourism): Array<TourismObj> {
+  var id = 0;
+  return tourism.reduce((acc, item) => {
+    id++;
+    acc.push({ index: id, title: item });
+    return acc;
+  }, <Array<TourismObj>>[]);
+}
+
+const masyvas = toObject(tourism);
+
+let htmlResult: string = '';
+masyvas.forEach((item: TourismObj) => {
+  htmlResult += `id: ${item.index}, title: ${item.title}<br>`;
+});
+
+//13 užduotis
+const arr = toObject(tourism);
+
+type VisitedPlaces = Array<number>;
+const VisitedPlaces: VisitedPlaces = [3, 5, 7];
+
+let htmlResVisited: string = '';
+arr.forEach((item: TourismObj) => {
+  if (VisitedPlaces.indexOf(item.index) > -1) {
+    htmlResVisited += `id: ${item.index}, title: <s>${item.title}</s><br> `;
+  } else {
+    htmlResVisited += `id: ${item.index}, title: ${item.title}<br> `;
+  }
+});
+
+const el = document.getElementById('masyvas');
+if (el) el.innerHTML = htmlResult;
+
+const el1 = document.getElementById('visitedPlaces');
+if (el1) el1.innerHTML = htmlResVisited;
+
+// 12 užduotis
+function getJustTourism(tourism: Tourism, turizmas: Tourism): Tourism {
+  const SetTour = tourism.filter((place) => {});
+  return SetTour;
+}
+var Turizmas = ['turizmo'];
+
 const result = getLithuaniaPlaces(tourism, Template);
 const noNumbers = arrayWithoutNumbers(tourism);
 const castles = getCastles(tourism);
@@ -83,3 +133,5 @@ document.getElementById('app')?.append(result.toString());
 document.getElementById('beSkaiciu')?.append(noNumbers.toString());
 document.getElementById('pilys')?.append(castles.toString());
 document.getElementById('simboliai')?.append(symbols.toString());
+document.getElementById('masyvas')?.append(masyvas.toString());
+document.getElementById('turizmas')?.append(Turizmas.toString());
--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var sum = addNumbers(10, 15);
 document.getElementById('app')?.append('Sum of the two numbers is : ' + sum);
 console.log('Sum of the two numbers is: ' + sum);
 */
-var _a, _b, _c, _d;
+var _a, _b, _c, _d, _e, _f;
 var tourism = [
     '1',
     'Traku_pilis',
@@ -69,6 +69,43 @@ var symbolCount = function (tourism) {
     });
     return symbols;
 };
+function toObject(tourism) {
+    var id = 0;
+    return tourism.reduce(function (acc, item) {
+        id++;
+        acc.push({ index: id, title: item });
+        return acc;
+    }, []);
+}
+var masyvas = toObject(tourism);
+var htmlResult = '';
+masyvas.forEach(function (item) {
+    htmlResult += "id: " + item.index + ", title: " + item.title + "<br>";
+});
+//13 užduotis
+var arr = toObject(tourism);
+var VisitedPlaces = [3, 5, 7];
+var htmlResVisited = '';
+arr.forEach(function (item) {
+    if (VisitedPlaces.indexOf(item.index) > -1) {
+        htmlResVisited += "id: " + item.index + ", title: <s>" + item.title + "</s><br> ";
+    }
+    else {
+        htmlResVisited += "id: " + item.index + ", title: " + item.title + "<br> ";
+    }
+});
+var el = document.getElementById('masyvas');
+if (el)
+    el.innerHTML = htmlResult;
+var el1 = document.getElementById('visitedPlaces');
+if (el1)
+    el1.innerHTML = htmlResVisited;
+// 12 užduotis
+function getJustTourism(tourism, turizmas) {
+    var SetTour = tourism.filter(function (place) { });
+    return SetTour;
+}
+var Turizmas = ['turizmo'];
 var result = getLithuaniaPlaces(tourism, Template);
 var noNumbers = arrayWithoutNumbers(tourism);
 var castles = getCastles(tourism);
@@ -77,3 +114,5 @@ var symbols = symbolCount(tourism);
 (_b = document.getElementById('beSkaiciu')) === null || _b === void 0 ? void 0 : _b.append(noNumbers.toString());
 (_c = document.getElementById('pilys')) === null || _c === void 0 ? void 0 : _c.append(castles.toString());
 (_d = document.getElementById('simboliai')) === null || _d === void 0 ? void 0 : _d.append(symbols.toString());
+(_e = document.getElementById('masyvas')) === null || _e === void 0 ? void 0 : _e.append(masyvas.toString());
+(_f = document.getElementById('turizmas')) === null || _f === void 0 ? void 0 : _f.append(Turizmas.toString());
